e61ed9c4c55f30814b156338430b43182993fc76
wj5019 <45660555+wj5019@users.noreply.github.com>
2019-05-28 22:31:35 +0800

    二叉树的对称
    请实现一个函数，用来判断一颗二叉树是不是对称的。注意，如果一个二叉树同此二叉树的镜像是同样的，定义其为对称的

---
+++ Solution13.java
@@ -0,0 +1,30 @@
+public class Solution {
+    boolean isSymmetrical(TreeNode pRoot)
+    {
+        TreeNode node = getMirror(pRoot);
+        return isSymmetrical(pRoot,node);
+    }
+    boolean isSymmetrical(TreeNode pRoot,TreeNode node)
+    {
+        if(pRoot == null && node == null){
+            return true;
+        }else if(pRoot == null || node  == null){
+            return false;
+        }
+        if(pRoot.val == node.val){
+            return isSymmetrical(pRoot.left,node.left)&&isSymmetrical(pRoot.right,node.right);
+        }
+       return false;
+    }
+     
+    TreeNode getMirror(TreeNode pRoot){
+        if (pRoot == null) {
+            return null;
+        }
+        TreeNode root = new TreeNode(pRoot.val);
+        root.right = getMirror(pRoot.left);
+        root.left = getMirror(pRoot.right);
+        return root;
+    }
+  
+}
