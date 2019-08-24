0fc502c204ce4fd8b322b6ffb3099fa559119391
wj5019 <45660555+wj5019@users.noreply.github.com>
2019-08-24 22:51:23 +0800

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -6,6 +6,7 @@ public class BubbleSort {
 			arr[i]=(int)(Math.random()*100);
 			System.out.println(arr[i]);
 		}
+		
 		for (int i = 0; i < arr.length-1; i++) {
 			for (int j = 0; j < arr.length-1-i; j++) {
 				if (arr[j]>arr[j+1]) {
@@ -16,6 +17,7 @@ public class BubbleSort {
 				
 			}
 		}
+		
 		System.out.println("排序后");
 		for (int i = 0; i < arr.length; i++) {
 			System.out.println(arr[i]);
