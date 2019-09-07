fe27b341deb92047cfc010e85ae220d63cb63162
wj5019 <45660555+wj5019@users.noreply.github.com>
2019-09-07 22:56:48 +0800

    Delete Solution3.java

--- Solution3.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigInteger;
-public class Solution {
-    public int Add(int num1,int num2) {
-        BigInteger bi1=new BigInteger(String.valueOf(num1));
-        BigInteger bi2=new BigInteger(String.valueOf(num2));
-        return bi1.add(bi2).intValue();
- 
-    }
-}
